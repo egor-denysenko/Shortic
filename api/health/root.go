c324e07b578354124094b7764389fe30cd04f3ae
egor-denysenko <75579636+egor-denysenko@users.noreply.github.com>
2022-08-06 12:18:33 +0200

    Implemented data endpoint and refactored health root endpoint

--- api/health/root.go
+++ api/health/root.go
@@ -1,14 +1,13 @@
 package health_root
 
 import (
-	"fmt"
 	"net/http"
 	apiUtility "shortic/api/utility"
 )
 
 func RootHealth (w http.ResponseWriter, r *http.Request) {
   if !apiUtility.ApiVersionHeaderCheck(r,"0.0.1"){
-    fmt.Fprintln(w,"Wrong api version")
+    w.WriteHeader(http.StatusGone)
     return
   }
   w.Write([]byte("Welcome from Shortic!"))
